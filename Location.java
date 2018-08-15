d19bfbb5f01724482fd34f92a148e1834ef5762b
DuskZhang <rzhang20001121@gmail.com>
2018-08-14 19:08:49 -0600

    Witchwood v1 - too slow

---
+++ Location.java
@@ -0,0 +1,18 @@
+
+public class Location {
+	private double probGet;
+	private int time;
+	public Location(double probGet, int time) {
+		this.probGet = probGet;
+		this.time = time;
+	}
+	
+	public double getProb() {
+		return probGet;
+	}
+	public int getTime() {
+return time;
+	}
+	
+}
+
